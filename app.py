c3df004c877d0ee86f8625f8b144941241a56758
kristinn <kristinnk@beholder.(none)>
2013-07-15 01:50:05 +0000

    Version 0.1, first version that can be marked as working.

--- app.py
+++ app.py
@@ -2,29 +2,98 @@ import web
 
 urls = (
     '/', 'index',
-    '/new', 'new'
+    '/admin', 'admin',
+    '/new', 'new',
+    '/edit/(\d+)', 'edit',
+    '/edit', 'edit',
+    '/delete/(\d+)', 'confirmdelete',
+    '/dodelete/(\d+)', 'performdelete',
+    '/login', 'login',
+    '/logout', 'logout'
 )
 
+web.config.debug = False
 app = web.application( urls, globals() )
 render = web.template.render('templates/', base='base')
 db = web.database(dbn='mysql', db='kBlog', user='kiddi', pw='beholder')
+session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'username': 'none'})
+
+def authenticate():
+	if session.username == "kiddi":
+		#if session.username != "kiddi":
+		web.debug( session.username )
+		pass
+	else:
+		web.debug("authenticate failed")
+		raise web.seeother('/login')
 
 class index(object):
     def GET(self):
     	items = db.select('posts')
         return render.index(items)
 
-class new(object):	
-	newPostForm = web.form.Form(
-			web.form.Textbox('title'),
-			web.form.Textarea('content'),
-			web.form.Checkbox('publish'),
-			web.form.Button('post')
-			)
+
+class admin(object):
+	def GET(self):
+		authenticate()
+		items = db.select('posts')
+		return render.admin(items, session)
+
+
+class confirmdelete(object):
+	def GET(self, id_number):
+		authenticate()
+		return render.confirm(id_number)
+
+
+class performdelete(object):
+	def GET(self, id_number):
+		db.delete('posts', where="id=$id_number", vars=locals())
+		raise web.seeother('/admin')
+
+
+class login(object):
+	loginForm = web.form.Form(
+		web.form.Textbox('username'),
+		web.form.Password('password'),
+		web.form.Button('Log in')
+		)
 
 	def GET(self):
-		np = self.newPostForm()
-		return render.admin( np )
+		li = self.loginForm()
+		return render.login( li )
+
+	def POST(self):
+		li = web.input()
+		user = db.select('users', where='username=$li.username and password=$li.password', vars=locals())
+		if user:
+			# return user[0].username
+			session.username = user[0].username
+			web.debug("found, setting session")
+			raise web.seeother('/admin')
+		else:
+			web.debug("not found")
+			raise web.seeother('/login')
+		
+
+class logout(object):
+	def GET(self):
+		session.kill()
+		raise web.seeother('/')
+
+
+class new(object):
+	postForm = web.form.Form(
+		web.form.Textbox('title'),
+		web.form.Textarea('content'),
+		web.form.Checkbox('publish'),
+		web.form.Button('post')
+		)
+
+	def GET(self):
+		authenticate()
+		np = self.postForm()
+		return render.new( np )
 
 	def POST(self):
 		np = web.input()
@@ -34,7 +103,34 @@ class new(object):
 		else:
 			publish_checkbox = 0;
 		db.insert('posts', title=np.title, content=np.content, author="Kiddi", publish=publish_checkbox )
-		raise web.seeother('/new')
+		raise web.seeother('/admin')
+
+
+class edit(object):
+	postForm = web.form.Form(
+			web.form.Textbox('id', class_='disabledInput'),
+			web.form.Textbox('title'),
+			web.form.Textarea('content'),
+			web.form.Checkbox('publish'),
+			web.form.Button('post')
+			)
+
+	def GET(self, id_number):
+		authenticate()
+		item = db.select('posts', where='id=$id_number', vars=locals())
+		ep = self.postForm()
+		ep.fill( item[0] )
+		return render.edit( ep )
 		
+	def POST(self):
+		np = web.input()
+		if np.has_key('publish'):
+			publish_checkbox = 1;
+		else:
+			publish_checkbox = 0;
+		db.update('posts', where="id=$np.id", vars=locals(), title=np.title, content=np.content, author="Kiddi", publish=publish_checkbox )
+		raise web.seeother('/admin')
+
+
 if __name__ == "__main__": 
     app.run()
--- sql/genesis.sql
+++ sql/genesis.sql
@@ -12,6 +12,19 @@ CREATE TABLE posts(
 	primary key (id)
 );
 
+CREATE TABLE users(
+	id INT AUTO_INCREMENT,
+	username TEXT,
+	password TEXT,
+	primary key (id)
+);
+
+INSERT INTO users VALUES(
+	NULL,
+	'kiddi',
+	'beholder'
+);
+
 INSERT INTO posts VALUES(
 	NULL,
 	'Post one',
@@ -27,13 +40,7 @@ INSERT INTO posts VALUES(
 	'Post two',
 	'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eget tellus gravida, porta dui nec, tristique sem. Ut malesuada dui ac mattis porta. Curabitur vehicula volutpat neque, non adipiscing magna ultrices vel. Fusce pretium in enim et ultrices. Praesent lacinia auctor metus. Phasellus adipiscing odio id elit ultricies lacinia. Donec venenatis diam in justo commodo, non tempor est porttitor. Curabitur nec velit ac diam posuere rhoncus. Suspendisse scelerisque mi id facilisis faucibus. Proin eu feugiat odio. Praesent luctus, libero ut venenatis facilisis, sapien lacus rhoncus ipsum, eget vehicula lacus elit at lorem. Phasellus sollicitudin enim urna, eu auctor augue gravida a. Mauris semper, dolor eget pellentesque fermentum, tellus sem bibendum neque, at feugiat mauris tellus ut est. Quisque tincidunt dolor non magna pretium vestibulum. Duis sit amet ipsum malesuada, fermentum ipsum sed, imperdiet lorem. Mauris scelerisque sem ut diam ultrices elementum.
 
-Phasellus erat turpis, consequat vel mi ac, bibendum consequat augue. Vestibulum mi ipsum, varius sit amet lorem ut, dapibus tincidunt nulla. Maecenas fermentum, eros sed facilisis porttitor, lorem lacus sodales mi, nec volutpat magna erat id lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id pretium purus. Aliquam erat volutpat. In consequat eu metus ut ultricies. Etiam et risus dignissim, cursus massa eget, auctor est. Mauris dapibus justo nibh, scelerisque interdum lacus ultricies non. Ut varius lorem et dui dignissim, sed adipiscing odio tincidunt. Praesent sagittis pulvinar tortor eget varius. Duis tempor ligula urna, ac sodales ligula ornare nec. Quisque malesuada nec turpis sit amet rutrum.
-
-Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus eu ipsum porttitor nisl eleifend pellentesque vel at justo. Praesent tincidunt hendrerit ante sed scelerisque. Nam dictum nulla non magna porttitor, nec ullamcorper sem aliquet. Donec dictum venenatis rutrum. Curabitur ut lorem sed turpis ultrices sollicitudin pretium rhoncus ipsum. Maecenas eu lectus leo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam nec orci lorem.
-
-Donec cursus turpis id orci placerat porta. Integer aliquam pretium luctus. Nullam non mauris nec tellus sagittis placerat vel a mi. Aliquam erat volutpat. Nulla adipiscing vehicula adipiscing. Donec commodo elementum felis malesuada imperdiet. Duis neque metus, rhoncus sit amet augue quis, dignissim iaculis elit. Nam at elit nec magna consequat egestas. Etiam quis tellus tincidunt orci dignissim dictum. Vivamus ac placerat massa. Vestibulum id tincidunt nisi, id iaculis dolor.
-
-Sed tincidunt interdum lacus sed luctus. Etiam velit erat, sagittis sed neque vitae, tristique tempus felis. Suspendisse semper ipsum id purus lacinia, vitae condimentum sem aliquet. Nunc accumsan bibendum commodo. Duis quis ligula nisi. In sollicitudin metus vel pulvinar aliquam. Fusce rutrum faucibus magna, quis porta elit iaculis in. Duis ante quam, volutpat ut dapibus eu, consectetur vitae tellus. Nam eu molestie ante. Quisque vel massa id lacus euismod volutpat.',
+Phasellus erat turpis, consequat vel mi ac, bibendum consequat augue. Vestibulum mi ipsum, varius sit amet lorem ut, dapibus tincidunt nulla. Maecenas fermentum, eros sed facilisis porttitor, lorem lacus sodales mi, nec volutpat magna erat id lorem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id pretium purus. Aliquam erat volutpat. In consequat eu metus ut ultricies. Etiam et risus dignissim, cursus massa eget, auctor est. Mauris dapibus justo nibh, scelerisque interdum lacus ultricies non. Ut varius lorem et dui dignissim, sed adipiscing odio tincidunt. Praesent sagittis pulvinar tortor eget varius. Duis tempor ligula urna, ac sodales ligula ornare nec. Quisque malesuada nec turpis sit amet rutrum.',
 	'Kiddi',
 	1,
 	NOW()
